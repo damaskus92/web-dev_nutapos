7b93b559beb01c217239dd979daeb496d2d5d2f6
damaskus92 <damaskusuma92@gmail.com>
2026-01-07 12:49:47 +0700

    feat: layouting

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
-<script setup></script>
-
 <template>
   <v-app>
-    <v-main>
-      <h1>You did it!</h1>
-      <p>
-        Visit
-        <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a>
-        to read the documentation
-      </p>
-
-      <v-btn variant="tonal">Button</v-btn>
+    <v-main class="bg-grey-lighten-3">
+      <v-container class="pa-6" fluid>
+        <router-view />
+      </v-container>
     </v-main>
   </v-app>
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-card rounded="xl" class="pa-2" style="box-shadow: 0 1px 3px rgba(0, 0, 0, 0.08)">
+    <v-card-title class="font-weight-semibold">Title</v-card-title>
+    <v-card-text> Content </v-card-text>
+  </v-card>
+</template>
+
+<script setup></script>
